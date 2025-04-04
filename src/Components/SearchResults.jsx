3856b4a424ad606c673274b1007d20bfbe6fcaad
tl345 <lethienan0511@gmail.com>
2025-04-04 16:59:39 -0500

    Result Page update, Mobile Screen update

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import "./SearchResults.css";
 
 function SearchResults() {
   const location = useLocation();
@@ -30,26 +31,36 @@ function SearchResults() {
   if (!results.players.length && !results.teams.length) return <p>No results found.</p>;
 
   return (
-    <div style={{ padding: "20px" }}>
+    <div className="search-results-box">
       <h2>Search Results for "{query}"</h2>
 
-      <h3>Players</h3>
-      <ul>
-        {results.players.map((player) => (
-          <li key={player._id}>
-            <Link to={`/player/${player.playerId}`}>{player.name}</Link>
-          </li>
-        ))}
-      </ul>
+      {results.players.length > 0 && (
+        <>
+          <h3>Players</h3>
+          <ul>
+            {results.players.map((player) => (
+              <li key={player._id}>
+                <Link to={`/player/${player.playerId}`}>{player.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
 
-      <h3>Teams</h3>
-      <ul>
-        {results.teams.map((team) => (
-          <li key={team._id}>
-            <Link to={`/team/${team.league}/${encodeURIComponent(team.name)}`}>{team.displayName}</Link>
-          </li>
-        ))}
-      </ul>
+      {results.teams.length > 0 && (
+        <>
+          <h3>Teams</h3>
+          <ul>
+            {results.teams.map((team) => (
+              <li key={team._id}>
+                <Link to={`/team/${team.league}/${encodeURIComponent(team.name)}`}>
+                  {team.displayName}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
